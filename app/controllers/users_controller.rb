041556eec9e9e43a7913afa490c30a877e8903bb
Ashal aka JOKER <ahastudio@gmail.com>
2025-11-14 03:18:48 +0900

    사용자 등록 API 구현
    POST /users 엔드포인트를 추가합니다.
    camelCase 파라미터를 받아 transform_keys로 변환합니다.
    ULID를 사용하여 사용자 ID를 생성합니다.
    
    사용자 생성 시 nickname을 name으로 자동 설정합니다.
    save! 메서드로 저장하고 예외를 rescue로 처리합니다.
    성공 시 201 상태코드와 id, username을 반환합니다.
    
    컨트롤러 테스트를 추가합니다.
    정상 케이스와 유효하지 않은 데이터 케이스를 테스트합니다.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    user.id = ULID.generate
+    user.nickname = user.name
+    user.save!
+
+    render json: { id: user.id, username: user.username },
+           status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages },
+           status: :unprocessable_entity
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :name, :phoneNumber)
+      .transform_keys(&:underscore)
+  end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "POST /users" do
+    assert_difference("User.count") do
+      post users_url, params: {
+        username: "newuser",
+        password: "password123",
+        name: "New User",
+        phoneNumber: "010-9999-8888"
+      }, as: :json
+    end
+
+    assert_response :created
+
+    assert_match "newuser", response.body
+  end
+
+  test "POST /users - with invalid data" do
+    assert_no_difference("User.count") do
+      post users_url, params: {
+        username: "",
+        password: "password123"
+      }, as: :json
+    end
+
+    assert_response :unprocessable_entity
+
+    assert_match "errors", response.body
+  end
+end
